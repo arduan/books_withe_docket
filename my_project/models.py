df08d1aa29f5b2306a13aa93a7e93e2e3a64468b
Vitaly <ivanov.arduan@yandex.ru>
2021-12-21 09:07:49 +0300

    get_url прописываем в модели.

--- my_project/models.py
+++ my_project/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from django.contrib.auth.models import User
 
@@ -6,6 +7,9 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField("Заголовок", max_length=200)
     text = models.TextField("Текст")
+
+    def get_url(self):
+        return reverse('title-text', args=[self.id])
 
     def __str__(self):
         return f'{self.title} {self.text}'
--- my_project/urls.py
+++ my_project/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 
     path('', views.show),
-    path('test/<int:id_title>', views.one_show),
+    path('test/<int:id_title>', views.one_show, name='title-text'),
 
 
 
