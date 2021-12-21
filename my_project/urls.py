007f866dbe1cbd75453d2c1379f0bd6efc5de4a0
Vitaly <ivanov.arduan@yandex.ru>
2021-12-21 08:42:20 +0300

    Продолжаю упражняться с шаблонами.

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-
-from my_project.views import *
 from . import views
 
 urlpatterns = [
 
-    path('', index, name='index'),
-    path('about/', about, name='about'),
-    path('test/', test, name='test'),
-    path('show/', views.show, name='example'),
+    path('', views.show),
+    path('test/<int:id_title>', views.one_show),
+
+
 
 
 
--- my_project/views.py
+++ my_project/views.py
@@ -5,34 +5,20 @@ from django.views.generic.base import TemplateView
 from .models import Post
 
 
-# Create your views here.
-
-
-def index(request):
-    """
-    :param request: Получат запрос и ...
-    :return: Возвращает в базовый шаблон base.html словарь data и отображает в шаблоне по ключу словаря.
-    """
-    data = {'name': 'Виталий', 'family': 'Иванов'}
-    return render(request, 'my_project/base.html', context=data)
-
-
 def test(request):
-    return render(request, 'my_project/test.html')
-
-
-def example(request):
-    return render(request, 'my_project/example.html')
-
-
-def about(request):
-    data = {'name': 'Виталий', 'family': 'Иванов'}
-    my_data = {'name': 'Светлана', 'family': 'Иванова'}
-    return render(request, 'my_project/about.html', context=my_data)
+    data = {'name': 'Эта страница test.html', 'family': 'Иванов'}
+    return render(request, 'my_project/test.html', context=data)
 
 
 def show(request):
     titles = Post.objects.all()
     return render(request, 'my_project/example.html', {
         'titles': titles,
+    })
+
+
+def one_show(request, id_title: int):
+    title = Post.objects.get(id=id_title)
+    return render(request, 'my_project/test.html', {
+        'title': title,
     })
