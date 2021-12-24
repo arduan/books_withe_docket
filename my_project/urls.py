1795031cdc8be0d776f7e40e2b04032fe034d7c0
Vitaly <ivanov.arduan@yandex.ru>
2021-12-24 10:05:56 +0300

    Пытаюсь разобраться в формах.

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+
+import forms
 from . import views
 
 urlpatterns = [
 
     path('', views.show),
     path('test/<int:id_title>', views.one_show, name='title-text'),
+    path('my_form/<int:id>', views.get_name, name='form'),
 
 
 
