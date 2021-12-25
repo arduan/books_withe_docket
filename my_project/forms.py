6f06f8728a4b1a298d87cda0211b75d8e1bf31fb
Vitaly <ivanov.arduan@yandex.ru>
2021-12-25 07:23:11 +0300

    Упражнения с формами.

--- my_project/forms.py
+++ my_project/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
+
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
+    age = forms.CharField(label='Возраст', max_length=4)
+    text = forms.CharField(label='Текст', max_length=200)
