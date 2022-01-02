ae42c2ad44898c79b5257c800165639b8a9cc2a1
Vitaly <ivanov.arduan@yandex.ru>
2022-01-02 09:27:18 +0300

    Первый воммит в этом году.

--- my_project/forms.py
+++ my_project/forms.py
@@ -2,4 +2,4 @@ from django.forms import modelform_factory, DecimalField
 
 from .models import Post
 
-form_model = modelform_factory(Post, fields=('title', 'text', 'age'))
+form_model = modelform_factory(Post, fields=('title', 'text', 'age', 'date_born'))
